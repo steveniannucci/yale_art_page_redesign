290b7ea7987a5d2df013dcecd42420bfb512225a
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-11-29 23:30:19 +0000

    Nov 29, 2024, 6:30 PM

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ document.getElementById('toggle-quick-links').addEventListener('click', function
     }
   });
 
+  const events = [
+  {
+    dateNumber: '15',
+    dateMonth: 'Oct',
+    eventName: 'Art Exhibition',
+    eventDate: 'October 15, 2024',
+    eventAddress: '123 Art St, New Haven, CT'
+  },
+  {
+    dateNumber: '20',
+    dateMonth: 'Nov',
+    eventName: 'Sculpture Workshop',
+    eventDate: 'November 20, 2024',
+    eventAddress: '456 Sculpture Rd, New Haven, CT'
+  },
+  // Add more events here
+];
+
   function populateEvents() {
   const container = document.querySelector('.happening-at-soa');
   
