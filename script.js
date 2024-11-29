1b99d2e87d44179af1e0089f65e1d8ec0096b7b1
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-11-29 21:56:14 +0000

    Nov 29, 2024, 4:56 PM

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById('toggle-quick-links').addEventListener('click', function
     var content = document.getElementById('quick-links-content');
     if (content.style.display === 'none') {
       content.style.display = 'block';
-      this.textContent = 'Hide Links';
+      this.textContent = 'Hide Quick Links';
     } else {
       content.style.display = 'none';
-      this.textContent = 'Show Links';
+      this.textContent = 'Show Quick Links';
     }
   });
