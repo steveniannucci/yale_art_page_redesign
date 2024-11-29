ebb6d08e8ecec464ae2ada23dd80d6dc470aed76
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-11-29 23:47:20 +0000

    Nov 29, 2024, 6:47 PM

--- script.js
+++ script.js
@@ -45,6 +45,13 @@ document.getElementById('toggle-quick-links').addEventListener('click', function
     eventDate: 'Sculpture critiques during the Fall 2024 semester are open to the School of Art community.',
     eventAddress: '36 Edgewood Ave., New Haven 12:00-3:00 PM EST'
   },
+  {
+    dateNumber: '03',
+    dateMonth: 'Dec',
+    eventName: 'Presentation of MFA Crit Work in Painting/Printmaking',
+    eventDate: '1-3 PM & 7-9 PM 353 Crown Street, the Pit Tuesday, December 3 Nadia Younes While...',
+    eventAddress: '353 Crown Street, the Pit 1:00-9:00 PM EST'
+  },
 ];
 
   function populateEvents() {
