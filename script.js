82107601d3864238cdb8a24cd07ce15bccf53447
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-11-30 23:13:49 +0000

    Nov 30, 2024, 6:13 PM

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.getElementById('toggle-quick-links').addEventListener('click', function
     eventDate: 'Fall 2024 1st-Year MFA Show November 13 - December 13 Public reception: Friday, November 13',
     eventAddress: '1156 Chapel Street',
     eventCategory: 'Incoming Students (1st Year MFAs), Open to Yale Community, Exhibitions',
-    additionalEventInfo: 'Fall 2024 1st-Year MFA Show\n\nNovember 13 - December 13',
+    additionalEventInfo: '',
     color: '#008080'
   },
   {
