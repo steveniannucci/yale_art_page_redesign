d00cb98acd04bf67fc7efac43ae938da6add4d0d
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-11-30 00:06:50 +0000

    Nov 29, 2024, 7:06 PM

--- script.js
+++ script.js
@@ -39,7 +39,8 @@ document.getElementById('toggle-quick-links').addEventListener('click', function
     dateMonth: 'Dec',
     eventName: 'Figure Drawing Workshop',
     eventDate: 'Monday, December 26-8 PM 1156 Chapel Street, Room G01 Join the Painting/Printmaking department...',
-    eventAddress: '5:00 PM - 6:00 PM EST'
+    eventAddress: '5:00 PM - 6:00 PM EST',
+    color: '#0C1446'
   },
   {
     dateNumber: '03',
