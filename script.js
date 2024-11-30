3d56afab2882ba74021943ecf6f22741b1478c74
StevenIannucci <89707282+StevenIannucci@users.noreply.github.com>
2024-11-30 22:55:41 +0000

    Nov 30, 2024, 5:55 PM

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ document.getElementById('toggle-quick-links').addEventListener('click', function
     eventName: 'Presentation of MFA Crit Work in Painting/Printmaking',
     eventDate: '1-3 PM & 7-9 PM 353 Crown Street, the Pit Tuesday, December 3 Nadia Younes While...',
     eventAddress: '353 Crown Street, the Pit 1:00-9:00 PM EST',
-    eventCategory: 'MFA Crits',
+    eventCategory: 'MFA Crits, Painting/Printmaking, Open to SoA Community',
     color: '#2B6AD0'
   },
 ];
